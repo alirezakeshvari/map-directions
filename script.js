45fdf3f2f7d38661c6795acf2164dc048b91d984
alirezakeshvari <alireza.ksv@gmail.com>
2022-01-26 14:33:27 +0330

    import map

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWxpcmV6YWt2ciIsImEiOiJja3l2ZXlzd2gxd3NpMnBvM3hhd3JtYzdoIn0.hton60MqrEQ9Vg6-e_NzSg";
+
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
